e23c7bf95564ab653f33a8ed6e062d296cb42303
Jason Ainsworth <jasoncruze@gmail.com>
2017-07-21 16:18:06 -0700

    add in versioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
-  resources :parks
+  namespace :api do
+    namespace :v1 do
+      resources :parks
+    end
+  end
+
 
   get '/park/random' => 'parks#random'
 
