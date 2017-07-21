47cda6a8c7effc88203691dda9aa5ffda45850e7
Jason Ainsworth <jasoncruze@gmail.com>
2017-07-21 16:05:08 -0700

    add in passing test for random park route

---
+++ spec/requests/get_random_park_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "get all parks route", :type => :request do
+  let!(:parks) { FactoryGirl.create_list(:park, 10)}
+
+  before { get '/park/random' }
+
+  it 'returns a random park' do
+    park = Park.find(JSON.parse(response.body)['id'])
+    expect(park).to have_attributes(:name => "#{park.name}")
+  end
+
+  it 'returns status code 200' do
+    expect(response).to have_http_status(:success)
+  end
+
+end
